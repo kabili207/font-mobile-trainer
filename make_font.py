be0c1f8d08922bb9ace1538eba7345ff106f4692
Amy Nagle <kabi-git@openmuffin.com>
2024-07-26 18:22:27 -0400

    Combine font generation scripts

--- make_font.py
+++ make_font.py
@@ -1,4 +1,7 @@
 import fontforge
+import subprocess
+import os
+import unicodedata
 
 hw_chars = (
 " !\"#$%&'()*+,-./"
@@ -7,9 +10,11 @@ hw_chars = (
 "PQRSTUVWXYZ[\\]^_"
 "`abcdefghijklmno"
 "pqrstuvwxyz{|}~"
+# Mobile Trainer does not support the half-width kana
 )
 
-
+# The first half of these are typically rendered as half-width
+# on most systems, but Mobile Trainer renders them as full-width
 fw_chars = (
 "¢£§¨¬°±´¶×÷Α"
 "ΒΓΔΕΖΗΘΙΚΛΜΝ"
@@ -57,31 +62,67 @@ fw_chars = (
 "ｙｚ｛｜｝￣￥"
 )
 
-mkr = fontforge.font()
-mkr.ascent = 1000
-mkr.descent = 200
-mkr.encoding = "Unicode"
+root = os.path.dirname(os.path.abspath(__file__))
+glyph_root = os.path.join(root, 'glyphs')
 
-sprite_root = '/home/kabili/Projects/Fonts/REON/MobileTrainer/glyphs/'
+if not os.path.exists(glyph_root):
+	os.makedirs(glyph_root)
 
-for i in range(0, len(hw_chars)):
-	s = hw_chars[i]
-	if not s in mkr:
-		gl = mkr.createChar(ord(s))
-	else:
-		gl = mkr[s]
-	sprite = "uni%04X.png" % ord(s)
-	gl.importOutlines(sprite_root + sprite)
-	gl.autoTrace()
-	gl.width = 600
+subprocess.run([
+	'magick', 'convert', os.path.join(root, 'jis_half.png'),
+	'-crop', '6x12', '+repage', '+adjoin',
+	os.path.join(glyph_root, 'half%d.png')
+	])
 	
-for i in range(0, len(fw_chars)):
-	s = fw_chars[i]
-	if not s in mkr:
-		gl = mkr.createChar(ord(s))
+subprocess.run([
+	'magick', 'convert', os.path.join(root, 'jis_full.png'),
+	'-crop', '12x12', '+repage', '+adjoin',
+	os.path.join(glyph_root, 'full%d.png')
+	])
+
+font = fontforge.font()
+font.ascent = 1000
+font.descent = 200
+font.encoding = "Unicode"
+	
+total_chars = len(hw_chars) + len(fw_chars)
+curr_index = 0
+
+def add_character(char, index, is_full):
+	global curr_index
+	if not char in font:
+		gl = font.createChar(ord(char))
 	else:
-		gl = mkr[s]
-	sprite = "uni%04X.png" % ord(s)
-	gl.importOutlines(sprite_root + sprite)
+		gl = font[char]
+	if is_full:
+		gl.width = 1200
+		path = 'full%d.png'
+	else:
+		gl.width = 600
+		path = 'half%d.png'
+	gl.importOutlines(os.path.join(glyph_root, path % index))
+	name = unicodedata.name(char)
+	curr_index += 1
+	print(f'[{curr_index:>3}/{total_chars}] Tracing {name}...')
 	gl.autoTrace()
-	gl.width = 1200
+	gl.validate()
+
+for i, c in enumerate(hw_chars):
+	add_character(c, i, False)
+	
+for i, c in enumerate(fw_chars):
+	add_character(c, i, True)
+
+# Keep these down here, they seem to mess with the image tracing
+font.upos = -150
+font.uwidth = 100
+
+font.encoding = 'compacted'
+font.fontname = "MobileTrainer" # no spaces!
+font.fullname = "Mobile Trainer"
+font.familyname = "Mobile Trainer"
+font.version = '1.01'
+font.copyright = 'Copyright (c) 2001 Nintendo.'
+
+font.save("MobileTrainer.sfd")
+font.close()
--- split_sheets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-magick convert jis_half.png -crop 6x12 +repage +adjoin glyphs/half%d.png
-magick convert jis_full.png -crop 12x12 +repage +adjoin glyphs/full%d.png
